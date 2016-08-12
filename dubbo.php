a034ef785cdf16aa85493f516e50cfbf73095c59
Sakura Wong <cloud976@qq.com>
2016-08-12 17:12:11 +0800

    fix bug

--- dubbo.php
+++ dubbo.php
@@ -1,93 +1,160 @@
 <?php
 namespace xiaofeng\dubbo;
+
 error_reporting(E_ALL);
 
-function connect($remote_socket, &$err = null) {
-	$fd = stream_socket_client("tcp://$remote_socket", $errno, $errstr, 5);
-	stream_set_timeout($fd, 5, 0);
-	if($fd === false) {
-		$err = "$errstr ($errno)";
-	}
-	return $fd;
+function connect($remote_socket, &$err = null)
+{
+    $fd = stream_socket_client("tcp://$remote_socket", $errno, $errstr, 5);
+    stream_set_timeout($fd, 5, 0);
+    if ($fd === false) {
+        $err = "$errstr ($errno)";
+    }
+    return $fd;
 }
 
-function serv_list($fd) {
-	if(!is_resource($fd)) {
-		return [];
-	}
-	$serv_arr = _exec_cmd($fd, "ls", true);
-	$result = [];
-	foreach($serv_arr as $serv) {
-		$serv = trim($serv);
-		$detail_arr = _exec_cmd($fd, "ls -l $serv", true);
-		$result[$serv] = array_map(function($m) { return _method_parse($m); }, $detail_arr);
-	}
-	return $result;
+function serv_list($fd)
+{
+    if (!is_resource($fd)) {
+        return [];
+    }
+    $serv_arr = _exec_cmd($fd, "ls", true);
+    $result = [];
+    foreach ($serv_arr as $serv) {
+        $serv = trim($serv);
+        $detail_arr = _exec_cmd($fd, "ls -l $serv", true);
+        $result[$serv] = array_map(function ($m) {return _method_parse($m);}, $detail_arr);
+    }
+    return $result;
 }
 
-function invoke($fd, $serv, $method, array $args = [], $pretty_print = false) {
-	if(!is_resource($fd)) {
-		return "无效连接~";
-	}
-	$arg_str = implode(", ", $args);
-	$receive = _exec_cmd($fd, "invoke $serv.$method($arg_str)");
-	$receive = str_replace("dubbo>", "", $receive);
-	if(function_exists("iconv")) {
-		$receive = iconv("GB2312", "UTF-8", $receive);
-	}
-	if($pretty_print) {
-		$ret_arr = explode("\r\n", $receive);
-		if(!isset($ret_arr[1])) return $receive;
-		$result = json_decode($ret_arr[0], true);
-		if(!$result) return $receive;
-		$result["__"] = $ret_arr[1];
-		$pretty = json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-		return $pretty ?: $receive;
-	} else {
-		return $receive;
-	}
+function invoke($fd, $serv, $method, array $args = [], $pretty_print = false)
+{
+    if (!is_resource($fd)) {
+        return "无效连接~";
+    }
+    $arg_str = implode(", ", $args);
+    $receive = _exec_cmd($fd, "invoke $serv.$method($arg_str)");
+    $receive = str_replace("dubbo>", "", $receive);
+    if (function_exists("iconv")) {
+        $receive = iconv("GB2312", "UTF-8", $receive);
+    }
+    if ($pretty_print) {
+        $ret_arr = explode("\r\n", $receive);
+        if (!isset($ret_arr[1])) {
+            return $receive;
+        }
+
+        $result = json_decode($ret_arr[0], true);
+        if ($result===false||$result===true) {
+            return $receive;
+        }
+		$result = [
+			'result'=>json_decode($ret_arr[0], true),
+			'time'=>$ret_arr[1],
+		];
+        $pretty = json_readable_encode($result);
+        return $pretty;
+    } else {
+        return $receive;
+    }
 }
 
 // @http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
-function startsWith($haystack, $needle) {
-	return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+function startsWith($haystack, $needle)
+{
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
 }
-function endsWith($haystack, $needle) {
-	return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
-}
-
-function _exec_cmd($fd, $cmd, $array = false) {
-	if(!is_resource($fd)) {
-		return false;
-	}
-	fwrite($fd, trim($cmd) . "\n");
-
-	$receive = "";
-	// 以dubbo>结尾判断数据包完整
-	while(!endsWith($receive, "dubbo>")) {
-		$receive .= fread($fd, 1 << 13);
-	}
-
-	if($array) {
-		$arr = explode("\n", $receive);
-		array_pop($arr);
-		return $arr;
-	} else {
-		return $receive;
-	}
+function endsWith($haystack, $needle)
+{
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
 }
 
-function _method_parse($method_signature) {
-	static $method_pattern = '/^([a-zA-Z0-9|\.]*)\s([a-zA-Z0-9]*)\(([a-zA-Z0-9|\.|,]*)\)$/';
-	if(preg_match($method_pattern, trim($method_signature), $matches)) {
-		return [
-			"return" => $matches[1],
-			"method" => $matches[2],
-			"args"  => explode(",", $matches[3]),
-		];
-	} else {
-		// debug
-		// exit($method_signature);
-	}
-	return [];
+function _exec_cmd($fd, $cmd, $array = false)
+{
+    if (!is_resource($fd)) {
+        return false;
+    }
+    fwrite($fd, trim($cmd) . "\n");
+
+    $receive = "";
+    // 以dubbo>结尾判断数据包完整
+    while (!endsWith($receive, "dubbo>")) {
+        $receive .= fread($fd, 1 << 13);
+    }
+
+    if ($array) {
+        $arr = explode("\n", $receive);
+        array_pop($arr);
+        return $arr;
+    } else {
+        return $receive;
+    }
+}
+
+function _method_parse($method_signature)
+{
+    static $method_pattern = '/^([a-zA-Z0-9|\.]*)\s([a-zA-Z0-9]*)\(([a-zA-Z0-9|\.|,]*)\)$/';
+    if (preg_match($method_pattern, trim($method_signature), $matches)) {
+        return [
+            "return" => $matches[1],
+            "method" => $matches[2],
+            "args" => explode(",", $matches[3]),
+        ];
+    } else {
+        // debug
+        // exit($method_signature);
+    }
+    return [];
+}
+
+function json_readable_encode($in, $indent = 0, $from_array = false)
+{
+    $_myself = __FUNCTION__;
+    $_escape = function ($str)
+    {
+        return preg_replace("!([\b\t\n\r\f\"\\'])!", "\\\\\\1", $str);
+    };
+
+    $out = '';
+
+    foreach ($in as $key=>$value)
+    {
+        $out .= str_repeat("\t", $indent + 1);
+        $out .= "\"".$_escape((string)$key)."\": ";
+
+        if (is_object($value) || is_array($value))
+        {
+            $out .= "\n";
+            $out .= $_myself($value, $indent + 1);
+        }
+        elseif (is_bool($value))
+        {
+            $out .= $value ? 'true' : 'false';
+        }
+        elseif (is_null($value))
+        {
+            $out .= 'null';
+        }
+        elseif (is_string($value))
+        {
+            $out .= "\"" . $_escape($value) ."\"";
+        }
+        else
+        {
+            $out .= $value;
+        }
+
+        $out .= ",\n";
+    }
+
+    if (!empty($out))
+    {
+        $out = substr($out, 0, -2);
+    }
+
+    $out = str_repeat("\t", $indent) . "{\n" . $out;
+    $out .= "\n" . str_repeat("\t", $indent) . "}";
+
+    return $out;
 }
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 namespace xiaofeng;
+
 require __DIR__ . "/dubbo.php";
 error_reporting(E_ALL);
 date_default_timezone_set("Asia/Shanghai");
@@ -9,103 +10,107 @@ header("Content-type: text/html; charset=utf-8");
 // 1. 太长了的话木有滚轮
 // 2. 用.innerHTML设置datalist，渲染出错，把旧的数据渲染出来
 
-function get_host() {
-	$host = filter_input(INPUT_POST, "host", FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-	if(!$host) {
-		$host = filter_input(INPUT_COOKIE, "host", FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-	}
-	return $host ?: "127.0.0.1";
+function get_host()
+{
+    $host = filter_input(INPUT_POST, "host", FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+    if (!$host) {
+        $host = filter_input(INPUT_COOKIE, "host", FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+    }
+    return $host ?: "127.0.0.1";
 }
 
-function get_port() {
-	$port = filter_input(INPUT_POST, "port", FILTER_VALIDATE_INT);
-	if(!$port) {
-		$port = filter_input(INPUT_COOKIE, "port", FILTER_VALIDATE_INT);
-	}
-	return $port ?: "20880";
+function get_port()
+{
+    $port = filter_input(INPUT_POST, "port", FILTER_VALIDATE_INT);
+    if (!$port) {
+        $port = filter_input(INPUT_COOKIE, "port", FILTER_VALIDATE_INT);
+    }
+    return $port ?: "20880";
 }
 
-function get_post($k, $or = "") {
-	if(!isset($_POST[$k])) {
-		$_POST[$k] = $or;
-	}
-	return $_POST[$k];
+function get_post($k, $or = "")
+{
+    if (!isset($_POST[$k])) {
+        $_POST[$k] = $or;
+    }
+    return $_POST[$k];
 }
 
-function get_java_type_json($java_type) {
-	// 线上正好有个以前留下的nashorn的eval环境~~
-	// 木有的话直接 return "" , 一些参数需要手写了~~
-	static $url = "http://xxxxxxxx/debug/eval";
-	if(dubbo\startsWith($java_type, "java.")) {
-		return "";
-	}
+function get_java_type_json($java_type)
+{
+    // 线上正好有个以前留下的nashorn的eval环境~~
+    // 木有的话直接 return "" , 一些参数需要手写了~~
+    return "";
+    static $url = "http://xxxxxxxx/debug/eval";
+    if (dubbo\startsWith($java_type, "java.")) {
+        return "";
+    }
 
-	$payload = <<<JS
+    $payload = <<<JS
 (function(){
 	var LoadRate= Java.type("$java_type")
 	return 反正就是个bean2Map(new LoadRate())
 	// 其实最好是个fastjson啥的,直接转成json就好了
 }())
 JS;
-	$opts = ["http" => [
-		"method"  => "POST",
-		"header"  => "Content-Type:text/plain;charset=UTF-8",
-		"content" => $payload,
-	]];
-	$ctx = stream_context_create($opts);
-	$rec = file_get_contents($url, false, $ctx);
-	$keys = explode(",", trim($rec, " \t\n\r\0\x0B{}"));
-	if(count($keys) <= 1) {
-		return "";
-	}
-	$keys = array_map(function($a) { return trim(explode("=", $a)[0]); }, $keys);
-	$array = array_combine($keys, array_fill(0, count($keys), null));
-	return json_encode($array, JSON_PRETTY_PRINT);
+    $opts = ["http" => [
+        "method" => "POST",
+        "header" => "Content-Type:text/plain;charset=UTF-8",
+        "content" => $payload,
+    ]];
+    $ctx = stream_context_create($opts);
+    $rec = file_get_contents($url, false, $ctx);
+    $keys = explode(",", trim($rec, " \t\n\r\0\x0B{}"));
+    if (count($keys) <= 1) {
+        return "";
+    }
+    $keys = array_map(function ($a) {return trim(explode("=", $a)[0]);}, $keys);
+    $array = array_combine($keys, array_fill(0, count($keys), null));
+    return json_encode($array, JSON_PRETTY_PRINT);
 }
-
 
 // 连接
-if(isset($_POST["connect"])) {
-	$expire = time() + 60 * 60 * 24 * 30;
-	setcookie("host", get_host(), $expire);
-	setcookie("port", get_port(), $expire);
-	header("Refresh: 0;");
-	exit;
+if (isset($_POST["connect"])) {
+    $expire = time() + 60 * 60 * 24 * 30;
+    setcookie("host", get_host(), $expire);
+    setcookie("port", get_port(), $expire);
+    header("Refresh: 0;");
+    exit;
 }
-
 
 $disconnected = false;
 $remote_host = get_host() . ":" . get_port();
 $fd = @dubbo\connect($remote_host, $err);
-if($err) {
-	if(function_exists("iconv")) {
-		$err = iconv("GB2312", "UTF-8", $err);
-	}
-	$disconnected = true;
+if ($err) {
+    if (function_exists("iconv")) {
+        $err = iconv("GB2312", "UTF-8", $err);
+    }
+    $disconnected = true;
 }
 $title = $disconnected ? "<span class='error'>[DISCONNCETED]</span>" : "<span class='normal'>[CONNCETED]</span>";
 
 // 调用dubbo服务
-if(isset($_POST["invoke"])) {
-	echo dubbo\invoke($fd, get_post("serv"), get_post("serv_method"), get_post("args", []), true);
-	exit;
+if (isset($_POST["invoke"])) {
+    echo dubbo\invoke($fd, get_post("serv"), get_post("serv_method"), get_post("args", []), true);
+    exit;
 }
 
 $serv_list = dubbo\serv_list($fd);
 $entity_json = [];
-foreach($serv_list as $serv => &$methods) {
-	foreach($methods as &$method) {
-		foreach($method["args"] as $i => $arg) {
-			if(isset($entity_json[$arg])) {
-				$method["args_type"][$i] = $entity_json[$arg];
-			} else {
-				$json = get_java_type_json($arg);
-				$method["args_type"][$i] = $json;
-				$entity_json[$arg] = $json;
-			}
-		}
-	}
-	unset($method);
+foreach ($serv_list as $serv => &$methods) {
+    foreach ($methods as &$method) {
+        if (isset($method['args'])) {
+            foreach ($method["args"] as $i => $arg) {
+                if (isset($entity_json[$arg])) {
+                    $method["args_type"][$i] = $entity_json[$arg];
+                } else {
+                    $json = get_java_type_json($arg);
+                    $method["args_type"][$i] = $json;
+                    $entity_json[$arg] = $json;
+                }
+            }
+        }
+    }
 }
 unset($methods);
 
@@ -136,7 +141,10 @@ unset($methods);
 <body>
 <h2 class="title">dubbo-man <?php echo $title ?></h2>
 <div class="connect">
-	<?php if($disconnected) echo "<p class='error'>ERROR: $err</p>" ?>
+	<?php if ($disconnected) {
+    echo "<p class='error'>ERROR: $err</p>";
+}
+?>
 	<form method="post" action="" class="pure-form">
 		<fieldset>
 			<input type="hidden" name="connect" value="1">
@@ -146,7 +154,10 @@ unset($methods);
 		</fieldset>
 </form>
 </div>
-<div id="container" class="pure-g <?php if($disconnected) echo "hide" ?>">
+<div id="container" class="pure-g <?php if ($disconnected) {
+    echo "hide";
+}
+?>">
 	<div class="pure-u-1-2 left">
 		<form method="post" action="" id="dubbo_form" class="pure-form pure-form-stacked">
 			<fieldset>
@@ -159,9 +170,9 @@ unset($methods);
 			<datalist id="serv_list">
 				<?php foreach ($serv_list as $serv => $methods): ?>
 					<option
-						data-serv="<?php echo $serv?>"
-						value="<?php echo array_slice(explode(".", $serv), -1, 1)[0]?>"></option>
-				<?php endforeach ?>
+						data-serv="<?php echo $serv ?>"
+						value="<?php echo array_slice(explode(".", $serv), -1, 1)[0] ?>"></option>
+				<?php endforeach?>
 			</datalist>
 
 			<label for="method_input">Methods</label>
@@ -192,8 +203,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var $method_args = document.getElementById("method_args")
 	var $result = document.getElementById("result")
 
-	window.serv_list = <?php echo json_encode($serv_list)?>;
-	window.entity_json = <?php echo json_encode($entity_json)?>;
+	window.serv_list = <?php echo json_encode($serv_list) ?>;
+	window.entity_json = <?php echo json_encode($entity_json) ?>;
 
 	function get_serv() {
 		var val = $serv_input.value
